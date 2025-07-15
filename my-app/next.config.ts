f3d12b8c8430611670545938f4a26b56d81141ce
Rohit Pithani <rohitpithani@Rohits-MacBook-Pro.local>
2025-07-15 20:42:46 +0530

    Some updates

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.vercel.app', // Allow images from Vercel Blob for placeholders
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
